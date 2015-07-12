ebd49c4f6876b2a2ff2983601dc5c5579b9125b7
Saran Kumpasano <yokotobe@gmail.com>
2015-07-12 19:56:55 +0900

    string utility class

---
+++ application/libraries/y_utils/YString.php
@@ -0,0 +1,22 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class YString {
+
+    public function __construct($params){
+        // Do something with $params
+    }
+	
+	public function camelCase($str, array $noStrip = []){
+		// non-alpha and non-numeric characters become spaces
+		$str = preg_replace('/[^a-z0-9' . implode("", $noStrip) . ']+/i', ' ', $str);
+		$str = trim($str);
+		// uppercase the first character of each word
+		$str = ucwords($str);
+		$str = str_replace(" ", "", $str);
+		$str = lcfirst($str);
+ 
+		return $str;
+	}
+}
+
+?>
